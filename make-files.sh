25f73c2d7147ac7eebd9198cb84f3a059355e24e
silvinor <366673+silvinor@users.noreply.github.com>
2025-10-01 16:12:02 +1000

    updates to kings

--- make-files.sh
+++ make-files.sh
@@ -7,13 +7,13 @@ trap 'echo; echo "Interrupted, stopping..."; exit 130' INT TERM
 # Ensure output directory exists
 mkdir -p ./dist
 
-# # Loop from 1 to 54
-# for i in {1..54}
-# do
-#   printf -v n "%02d" "$i"
-#   echo "Generating card $n..."
-#   php card.php --card="$i" | rsvg-convert -f png -z 2 | magick png:- -background white -flatten -profile ./sRGB.icc -profile ./SWOP2006_Coated3v2.icc ./dist/card-$n.jpg
-# done
+# Loop from 1 to 54
+for i in {1..54}
+do
+  printf -v n "%02d" "$i"
+  echo "Generating card $n..."
+  php card.php --card="$i" | rsvg-convert -f png -z 2 | magick png:- -background white -flatten -profile ./sRGB.icc -profile ./SWOP2006_Coated3v2.icc ./dist/card-$n.jpg
+done
 
 # Loop from 1 to {however many back there are}
 for j in {1..13}
